394e5e03a4139f11c9e096ab6c3e7d718d18b603
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-15 21:02:06 +0300

    doing a commit

--- main.c
+++ main.c
@@ -13,7 +13,9 @@ int main(void)
 	int len2;
 	unsigned int ui;
 	void *addr;
-
+	/**
+	 * I am testing the c, s, and % specifiers here.
+	*/
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;

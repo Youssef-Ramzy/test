debebc516f679e1db92b3c5de474b84b577be75d
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-15 20:09:44 +0300

    H

--- R_functions.c
+++ R_functions.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include <unistd.h>
 /**
  * print_format - prints a string
  * @specifier: char to check
@@ -13,20 +14,35 @@ int print_format(char specifier, va_list ap)
 	switch (specifier)
 	{
 	case 'c':
-		count += _putchar(va_arg(ap, int));
+		count += _putchar(va_arg(ap, int)); /* to print char */
 		break;
 	case 's':
-		count += print_string(va_arg(ap, char *));
+		count += print_string(va_arg(ap, char *)); /* to print string */
 		break;
 	case '%':
-		count += _putchar('%');
+		count += _putchar('%'); /* to print % */
 		break;
 	case 'd':
 	case 'i':
-		count += print_integer(va_arg(ap, int));
+		count += print_integer(va_arg(ap, int)); /* to print integer */
 		break;
 	default:
 		break;
 	}
 	return (count);
+}
+/**
+ * print_string - prints a string
+ * @s: string to print
+ * Return: number of characters printed
+*/
+int print_string(char *s)
+{
+	int i;
+
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	return (i);
 }
